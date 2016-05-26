7b4ce237dbb47a16e49921492944b14ff71bbcb0
youxianqin <youxianqin@DESTOP-UFETQQ3>
2016-05-26 21:33:29 +0800

    add auth and copy templates from django.contirb.admin

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -79,12 +79,12 @@ DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-	'ENGINE': 'django.db.backends.mysql',
-	'NAME': 'blog',
-	'USER': 'testfordjango',
-	'PASSWORD': '123456',
-	'HOST': '127.0.0.1',
-	'PORT': '3306'
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'blog',
+		'USER': 'testfordjango',
+		'PASSWORD': '123456',
+		'HOST': '127.0.0.1',
+		'PORT': '3306'
     }
 }
 
